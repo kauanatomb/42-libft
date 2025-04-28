8494cadbd7d0c232b1278b82ab054accfaa79d18
Kauana Tombolato <kauanatomb@gmail.com>
2025-04-28 16:04:25 +0200

    strmapi

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -13,5 +13,20 @@
 
 char	*ft_strmapi(const char *s, char (*f)(unsigned int, char))
 {
+  char	*str;
+  size_t	i;
 
+  if (!s || !f)
+    return (NULL);
+  str = malloc(sizeof(char) * (ft_strlen(s) + 1));
+  if (!str)
+    return (NULL);
+  i = 0;
+  while (s[i])
+  {
+    str[i] = f(i, s[i]);
+    i++;
+  }
+  str[i] = '\0';
+  return (str);
 }
